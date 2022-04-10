3ee081b0875cd36efd5b245870830916d1704917
RodericAguilar <raguilar@elpuig.xeill.net>
2022-04-10 10:15:28 +0200

    Optimized
    Optimized Client, Server and Js, now it will be work good.

--- Client/Client.lua
+++ Client/Client.lua
@@ -1,50 +1,18 @@
 
-RegisterCommand(Config.CommandCash, function()
-    SendNUIMessage({
-        action = 'showwallet',
-        time = Config.TimeStatus
-    })
-end)
-
-RegisterCommand(Config.CommandBank, function()
-    SendNUIMessage({
-        action = 'showbank',
-        time = Config.TimeStatus
-    })
-end)
-
-RegisterCommand(Config.CommandVIPCoins, function()
-    SendNUIMessage({
-        action = 'showcoins',
-        time = Config.TimeStatus
-    })
-end)
-
-RegisterCommand(Config.CommandBlackMoney, function()
-    SendNUIMessage({
-        action = 'showblack',
-        time = Config.TimeStatus
-    })
-end)
    
-Citizen.CreateThread(function() 
-    RegisterNetEvent("5G-Hud:setValues")
-    AddEventHandler("5G-Hud:setValues", function(wallet, bank, coins, black_money)
-    
-    SendNUIMessage({
-        wallet = Config.IconMoneys .. wallet,
-        bank = Config.IconMoneys .. bank,
-        coins = Config.IconMoneys .. coins,
-        black_money = black_money
-    })
-    
-    end)
+RegisterNetEvent("5G-Hud:setValues")
+AddEventHandler("5G-Hud:setValues", function(account, money)
+    GetAccountAndSendMoney(account, money)
 end)
 
-    
-Citizen.CreateThread(function() 
-    while true do
-        Wait(Config.WaitValue)
-        TriggerServerEvent('5G-Hud:getMoneys')
-    end
-end)
+
+function GetAccountAndSendMoney(account, amount)
+    SendNUIMessage({
+        action = 'UpdateMoney',
+        account = account,
+        money = amount,
+        icon = Config.IconMoneys,
+        time = Config.TimeStatus
+    })
+end
+
