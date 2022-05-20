3aa461c80627fbe7fff0e049de2dba6cd8a9660e
amineohn <amineprojet7@gmail.com>
2022-05-20 08:17:04 +0200

    refactor(app): some refactoring things

--- html/js/app.js
+++ html/js/app.js
@@ -1,32 +1,37 @@
-$(function () {
-  window.addEventListener("message", function (event) {
-    var v = event.data;
-    switch (v.action) {
+$(() => {
+  window.addEventListener("message", (event) => {
+    const data = event.data;
+    switch (data.action) {
       case "UpdateMoney":
-        if (v.account == "money") {
-          $(".dwallet").text(`${v.icon} ${v.money}`);
-          $(".wallet").fadeIn();
-          setTimeout(() => {
-            $(".wallet").fadeOut();
-          }, v.time);
-        } else if (v.account == "bank") {
-          $(".dbank").text(`${v.icon} ${v.money}`);
-          $(".bank").fadeIn();
-          setTimeout(() => {
-            $(".bank").fadeOut();
-          }, v.time);
-        } else if (v.account == "coins") {
-          $(".dcoins").text(`${v.icon} ${v.money}`);
-          $(".coins").fadeIn();
-          setTimeout(() => {
-            $(".coins").fadeOut();
-          }, v.time);
-        } else if (v.account == "black_money") {
-          $(".dblack").text(`${v.money}`);
-          $(".black").fadeIn();
-          setTimeout(() => {
-            $(".black").fadeOut();
-          }, v.time);
+        switch (data.account) {
+          case "money":
+            $(".dwallet").text(`${data.icon} ${data.money}`);
+            $(".wallet").fadeIn();
+            setTimeout(() => {
+              $(".wallet").fadeOut();
+            }, data.time);
+            break;
+          case "bank":
+            $(".dbank").text(`${data.icon} ${data.money}`);
+            $(".bank").fadeIn();
+            setTimeout(() => {
+              $(".bank").fadeOut();
+            }, data.time);
+            break;
+          case "coins":
+            $(".dcoins").text(`${data.icon} ${data.money}`);
+            $(".coins").fadeIn();
+            setTimeout(() => {
+              $(".coins").fadeOut();
+            }, data.time);
+            break;
+          case "black_money":
+            $(".dblack").text(`${data.money}`);
+            $(".black").fadeIn();
+            setTimeout(() => {
+              $(".black").fadeOut();
+            }, data.time);
+            break;
         }
 
         break;
